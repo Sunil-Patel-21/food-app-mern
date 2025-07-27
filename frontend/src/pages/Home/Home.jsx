43603cbcff971cba4cd0b8258d92e7b292a7b924
Sunil-Patel-21 <patelsunil60273@gmail.com>
2025-07-27 17:17:37 +0530

    add header part

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import "./Home.css"
+import Header from '../../components/Header/Header'
 
 function Home() {
   return (
     <div>
-      Home Page 
+      <Header />
     </div>
   )
 }
