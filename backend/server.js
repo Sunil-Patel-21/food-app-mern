35a7375a84355b297d6c0d971cf4f1c27a560b72
Sunil-Patel-21 <patelsunil60273@gmail.com>
2025-07-30 20:38:05 +0530

    backend setup food models and routes

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
+dotenv.config();
+
+// app config
+const app = express();
+const port = process.env.PORT || 4000;
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+// app.use(express.urlencoded({extended:true}));
+
+// DB config
+connectDB();
+
+// api routes
+app.use("/api/food",foodRouter);
+
+
+
+app.get("/",(req,res)=>{
+    res.status(200).send("API Working");
+})
+
+app.listen(port,()=>{
+    console.log(`Listening to requests on ${port}`);
+})
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,7 +1,6 @@
 import React, { useContext } from "react";
 import { StoreContext } from "../../context/StoreContext";
 import "./Cart.css";
-import { food_list } from "../../assets/frontend_assets/assets";
 import { useNavigate } from "react-router-dom";
 function Cart() {
   const { cartItems,food_list, removeFromCart,getTotalCartAmount } = useContext(StoreContext);
@@ -50,12 +49,12 @@ function Cart() {
             <hr />
             <div className="cart-total-details">
               <p>Delivery fee</p>
-              <p>${2}</p>
+              <p>${getTotalCartAmount()===0?0:2}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>${getTotalCartAmount() +2}</b>
+              <b>${getTotalCartAmount()===0?0:getTotalCartAmount() +2}</b>
             </div>
           </div>
           <button onClick={() => navigate("/order")}>PROCEED TO CHECKOUT</button>
